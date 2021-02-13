e4d1ea5a50297a1c4ed0029c69abca1bb492872b
pmapelli <pmapelli@hotmail.com>
2021-02-13 10:54:47 -0300

    feat: Criando tela de perfil

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -41,8 +41,8 @@ const Dashboard: React.FC = () => {
   }, []);
 
   const navigateToProfile = useCallback(() => {
-    signOut();
-    // navigate('Profile');
+    // signOut();
+    navigate('Profile');
   }, [navigate]);
 
   const navigateToCreateAppointment = useCallback(
